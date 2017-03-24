fa2b9331189a81e46e4f1b1ce38987bbfbb17328
rmit-s3590771-Yuhao-Zhang <tirsky@zhangyuhaodeMacBook-Pro.local>
2017-03-24 11:19:40 +1100

    Driver-Menu

---
+++ src/com/APass1/rmit/Driver.java
@@ -0,0 +1,18 @@
+package com.APass1.rmit;
+
+import java.util.Scanner;
+
+public class Driver {
+	public void menu(){
+		System.out.println("Ozlypic Game");
+		System.out.println("======================================");
+		System.out.println("1. Select a game to run");
+		System.out.println("2. Predict the winner of the game");
+		System.out.println("3. Start the game");
+		System.out.println("4. Display the final result of results of all gomes");
+		System.out.println("5. Display the point of all athletes");
+		System.out.println("6. Exit");
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+}
