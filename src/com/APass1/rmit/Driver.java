a3ea5499c924207da8d08421c1c396ecbb5b069d
rmit-s3590771-Yuhao-Zhang <tirsky@zhangyuhaodeMacBook-Pro.local>
2017-03-26 21:38:06 +1100

    check user input

--- src/com/APass1/rmit/Driver.java
+++ src/com/APass1/rmit/Driver.java
@@ -3,7 +3,16 @@ package com.APass1.rmit;
 import java.util.Scanner;
 
 public class Driver {
-	public void menu(){
+	private int input;
+	private boolean error;
+	private Scanner sc = new Scanner(System.in);
+	
+	public void runGame(){
+		menu();
+		checkInput();
+	}
+	private void menu(){
+		//Create menu for game.
 		System.out.println("Ozlypic Game");
 		System.out.println("======================================");
 		System.out.println("1. Select a game to run");
@@ -12,7 +21,28 @@ public class Driver {
 		System.out.println("4. Display the final result of results of all gomes");
 		System.out.println("5. Display the point of all athletes");
 		System.out.println("6. Exit");
-		Scanner scanner = new Scanner(System.in);
-		
 	}
+	private void checkInput(){
+		//Check user's input if it is integer between 1 and 6.
+		do{
+			try{
+				int choice = sc.nextInt();
+				input = choice;
+				if(input > 0 && input <= 6){
+					error = false;
+				}else{
+					menu();
+					System.out.println("Your input is invalid. Please input an integer between 1 to 5.");
+					error = true;
+				}
+			}
+			catch(java.util.InputMismatchException e){
+				menu();
+				System.out.println("Your input is invalid. Please input an integer between 1 to 5.");
+				sc.next();
+				error = true;
+			}
+		}while(error);
+	}
+	
 }
---
+++ src/com/APass1/rmit/Ozlympic.java
@@ -0,0 +1,11 @@
+package com.APass1.rmit;
+
+public class Ozlympic {
+
+	public static void main(String[] args) {
+		Driver myDriver = new Driver();
+		myDriver.runGame();
+
+	}
+
+}
